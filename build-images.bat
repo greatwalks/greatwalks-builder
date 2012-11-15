23bbdaebb88d23460bdc98c6e5723c8b0588f9ed
Matthew Holloway <matth@catalyst.net.nz>
2012-11-16 11:13:17 +1300

    Automated commit

--- build-images.bat
+++ build-images.bat
@@ -1,3 +1,11 @@
+REM Generate icons in "greatwalks-phonegap" android project
+inkscape images/great-walks-icon.svg -z --export-png=../greatwalks-phonegap/res/drawable-hdpi/ic_launcher.png --export-width=72
+inkscape images/great-walks-icon.svg -z --export-png=../greatwalks-phonegap/res/drawable-ldpi/ic_launcher.png --export-width=36
+inkscape images/great-walks-icon.svg -z --export-png=../greatwalks-phonegap/res/drawable-mdpi/ic_launcher.png --export-width=48
+inkscape images/great-walks-icon.svg -z --export-png=../greatwalks-phonegap/res/drawable-xhdpi/ic_launcher.png --export-width=96
+
+
+REM Generate images in "greatwalks" repo
 mkdir "..\greatwalks\img"
 mkdir "..\greatwalks\img\walks"
 mkdir "..\greatwalks\img\walks\abel-tasman-coast-track"
@@ -19,41 +27,33 @@ inkscape images/great-walks-logo.svg -z --export-png=../greatwalks/img/logo-x300
 inkscape images/great-walks-logo.svg -z --export-png=../greatwalks/img/logo-x600.png --export-width=600
 inkscape images/great-walks-logo.svg -z --export-png=../greatwalks/img/logo-x1200.png --export-width=1200
 
-inkscape images/great-walks-icon.svg -z --export-png=../greatwalks-phonegap/res/drawable-hdpi/ic_launcher.png --export-width=72
-inkscape images/great-walks-icon.svg -z --export-png=../greatwalks-phonegap/res/drawable-ldpi/ic_launcher.png --export-width=36
-inkscape images/great-walks-icon.svg -z --export-png=../greatwalks-phonegap/res/drawable-mdpi/ic_launcher.png --export-width=48
-inkscape images/great-walks-icon.svg -z --export-png=../greatwalks-phonegap/res/drawable-xhdpi/ic_launcher.png --export-width=96
+resize -i"images/homepage-carousel/Abel Tasman Coast Track.jpg" -o"../greatwalks/img/homepage-carousel-x2048-abel-tasman-coast-track.jpg" -s2048
+resize -i"images/homepage-carousel/Rakiura Track - Stewart Island.jpg" -o"../greatwalks/img/homepage-carousel-x2048-rakiura-track---stewart-island.jpg" -s2048
+resize -i"images/homepage-carousel/Heaphy Track.jpg" -o"../greatwalks/img/homepage-carousel-x2048-heaphy-track.jpg" -s2048
+resize -i"images/homepage-carousel/Routeburn Track.jpg" -o"../greatwalks/img/homepage-carousel-x2048-routeburn-track.jpg" -s2048
+resize -i"images/homepage-carousel/Kepler Track.jpg" -o"../greatwalks/img/homepage-carousel-x2048-kepler-track.jpg" -s2048
+resize -i"images/homepage-carousel/Tongariro Northern Circuit.jpg" -o"../greatwalks/img/homepage-carousel-x2048-tongariro-northern-circuit.jpg" -s2048
+resize -i"images/homepage-carousel/Lake Waikaremoana.jpg" -o"../greatwalks/img/homepage-carousel-x2048-lake-waikaremoana.jpg" -s2048
+resize -i"images/homepage-carousel/Whanganui Journey.jpg" -o"../greatwalks/img/homepage-carousel-x2048-whanganui-journey.jpg" -s2048
+resize -i"images/homepage-carousel/Milford Track.jpg" -o"../greatwalks/img/homepage-carousel-x2048-milford-track.jpg" -s2048
+resize -i"images/homepage-carousel/_default.jpg" -o"../greatwalks/img/homepage-carousel-x2048-_default.jpg" -s2048
 
-
-resize -i"images/tongariro-home.jpg" -o"../greatwalks/img/tongariro-home.jpg" -s640x430
-resize -i"images/Abel-NEW-home.jpg" -o"../greatwalks/img/Abel-NEW-home.jpg" -s640x430
-resize -i"images/heaphy-home_0.jpg" -o"../greatwalks/img/heaphy-home_0.jpg" -s640x430
-resize -i"images/Kepler-NEW-home.jpg" -o"../greatwalks/img/Kepler-NEW-home.jpg" -s640x430
-resize -i"images/Milford-home.jpg" -o"../greatwalks/img/Milford-home.jpg" -s640x430
-resize -i"images/rakiura-home.jpg" -o"../greatwalks/img/rakiura-home.jpg" -s640x430
-resize -i"images/Routeburn-home.jpg" -o"../greatwalks/img/Routeburn-home.jpg" -s640x430
-resize -i"images/Waikaremoana-NEW-home.jpg" -o"../greatwalks/img/Waikaremoana-NEW-home.jpg" -s640x430
-resize -i"images/Whanganui-Journey-home.jpg" -o"../greatwalks/img/Whanganui-Journey-home.jpg" -s640x430
-
-copy /Y images\map-background.png ..\greatwalks\img\map-background.png
-
-copy /Y images\tongariro-home.jpg ..\greatwalks\img\tongariro-home@2x.jpg
-copy /Y images\Abel-NEW-home.jpg ..\greatwalks\img\Abel-NEW-home@2x.jpg
-copy /Y images\heaphy-home_0.jpg.jpg ..\greatwalks\img\heaphy-home_0@2x.jpg
-copy /Y images\Kepler-NEW-home.jpg ..\greatwalks\img\Kepler-NEW-home@2x.jpg
-copy /Y images\Milford-home.jpg ..\greatwalks\img\Milford-home@2x.jpg
-copy /Y images\rakiura-home.jpg ..\greatwalks\img\rakiura-home@2x.jpg
-copy /Y images\Routeburn-home.jpg ..\greatwalks\img\Routeburn-home@2x.jpg
-copy /Y images\Waikaremoana-NEW-home.jpg ..\greatwalks\img\Waikaremoana-NEW-home@2x.jpg
-copy /Y images\Whanganui-Journey-home.jpg ..\greatwalks\img\Whanganui-Journey-home@2x.jpg
+resize -i"images/homepage-carousel/Abel Tasman Coast Track.jpg" -o"../greatwalks/img/homepage-carousel-x1024-abel-tasman-coast-track.jpg" -s1024
+resize -i"images/homepage-carousel/Rakiura Track - Stewart Island.jpg" -o"../greatwalks/img/homepage-carousel-x1024-rakiura-track---stewart-island.jpg" -s1024
+resize -i"images/homepage-carousel/Heaphy Track.jpg" -o"../greatwalks/img/homepage-carousel-x1024-heaphy-track.jpg" -s1024
+resize -i"images/homepage-carousel/Routeburn Track.jpg" -o"../greatwalks/img/homepage-carousel-x1024-routeburn-track.jpg" -s1024
+resize -i"images/homepage-carousel/Kepler Track.jpg" -o"../greatwalks/img/homepage-carousel-x1024-kepler-track.jpg" -s1024
+resize -i"images/homepage-carousel/Tongariro Northern Circuit.jpg" -o"../greatwalks/img/homepage-carousel-x1024-tongariro-northern-circuit.jpg" -s1024
+resize -i"images/homepage-carousel/Lake Waikaremoana.jpg" -o"../greatwalks/img/homepage-carousel-x1024-lake-waikaremoana.jpg" -s1024
+resize -i"images/homepage-carousel/Whanganui Journey.jpg" -o"../greatwalks/img/homepage-carousel-x1024-whanganui-journey.jpg" -s1024
+resize -i"images/homepage-carousel/Milford Track.jpg" -o"../greatwalks/img/homepage-carousel-x1024-milford-track.jpg" -s1024
+resize -i"images/homepage-carousel/_default.jpg" -o"../greatwalks/img/homepage-carousel-x1024-_default.jpg" -s1024
 
 copy /Y images\header-icons.png ..\greatwalks\img\header-icons.png
 copy /Y images\slideout-menu.png ..\greatwalks\img\slideout-menu.png
-
 copy /Y images\table.png ..\greatwalks\img\table.png
 copy /Y images\weta_land.png ..\greatwalks\img\weta_land.png
 copy /Y images\weta_twitch.png ..\greatwalks\img\weta_twitch.png
-
 copy /Y images\map-icons\airport.png ..\greatwalks\img\icon-map-airport.png
 copy /Y images\map-icons\alert.png ..\greatwalks\img\icon-map-alert.png
 copy /Y images\map-icons\bird-or-animal-species.png ..\greatwalks\img\icon-map-bird-or-animal-species.png
@@ -74,7 +74,6 @@ copy /Y images\map-icons\volcano.png ..\greatwalks\img\icon-map-volcano.png
 copy /Y images\map-icons\waterfall.png ..\greatwalks\img\icon-map-waterfall.png
 copy /Y images\map-icons\swimming.png ..\greatwalks\img\icon-map-swimming.png
 copy /Y images\map-icons\side_trail.png ..\greatwalks\img\icon-map-side_trail.png
-
 copy /Y images\content-icons\calendar.png  ..\greatwalks\img\icon-content-calendar.png
 copy /Y images\content-icons\culture.png ..\greatwalks\img\icon-content-culture.png
 copy /Y images\content-icons\maps_blue.png ..\greatwalks\img\icon-content-maps_blue.png
@@ -88,13 +87,10 @@ copy /Y images\content-icons\canoe.png ..\greatwalks\img\icon-content-canoe.png
 copy /Y images\content-icons\lakes.png ..\greatwalks\img\icon-content-lakes.png
 copy /Y images\content-icons\mountains.png ..\greatwalks\img\icon-content-mountains.png
 copy /Y images\content-icons\specials.png ..\greatwalks\img\icon-content-specials.png
-
-
 copy /Y images\youarehere.png ..\greatwalks\img\youarehere.png
 copy /Y images\camera-placeholder.jpg ..\greatwalks\img\camera-placeholder.jpg
 copy /Y images\missing-icon.png ..\greatwalks\img\missing-icon.png
 copy /Y images\homepage-sign.png ..\greatwalks\img\homepage-sign.png
 copy /Y images\homepage-buttons.png ..\greatwalks\img\homepage-buttons.png
 copy /Y images\content-buttons.png ..\greatwalks\img\content-buttons.png
-
 copy /Y images\walk-icons.png ..\greatwalks\img\walk-icons.png
