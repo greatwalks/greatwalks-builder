656d9da26944bb34c7a898a907221945ecf08895
Matthew Holloway <matth@catalyst.net.nz>
2012-11-16 15:38:19 +1300

    Automated commit

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,16 @@
+git checkout master
 git add -A
 git commit -m "Automated commit"
 git push
 cd ../greatwalks
+git checkout master
 git add -A
 git commit -m "Automated commit"
 git push
+git checkout gh-pages
+git merge origin
+git push origin gh-pages
+git checkout master
 cd ../greatwalks-phonegap/assets/www
 git pull
 cd ../../../greatwalks-builder
