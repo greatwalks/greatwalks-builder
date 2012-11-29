258595d15185d4921e73a75f7f9d911bef298b79
Matthew Holloway <matth@catalyst.net.nz>
2012-11-30 11:26:13 +1300

    Automated commit

--- javascript/pageload.js
+++ javascript/pageload.js
@@ -3,7 +3,7 @@
 (function($){
     window.pageload = function(callback, pathname_prefix){
         if(pathname_prefix && window.location.pathname.toString().substr(0, pathname_prefix.length) !== pathname_prefix) return;
-        if (window.Media && navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
             document.addEventListener("deviceready", callback, false);
         } else {
             $(document).ready(callback);
