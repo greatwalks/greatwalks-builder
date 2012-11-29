de202f313eb3a75138027167c4aa9d800aa1fe4f
Matthew Holloway <matth@catalyst.net.nz>
2012-11-30 11:37:21 +1300

    Automated commit

--- javascript/pageload.js
+++ javascript/pageload.js
@@ -1,8 +1,8 @@
 /* globals window Modernizr Hammer */
 /* Simple click wrapper */
 (function($){
-    window.pageload = function(callback, pathname_prefix){
-        if(pathname_prefix && window.location.pathname.toString().substr(0, pathname_prefix.length) !== pathname_prefix) return;
+    window.pageload = function(callback, pathname_contains){
+        if(pathname_prefix && window.location.pathname.indexOf(pathname_prefix) === -1 ) return;
         if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
             document.addEventListener("deviceready", callback, false);
         } else {
