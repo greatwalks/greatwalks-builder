f843e1897c2ea570e72b78f042dc0e6315b27fdd
Matthew Holloway <matth@catalyst.net.nz>
2012-12-03 09:40:47 +1300

    Automated commit

--- bin/windows/deploy.bat
+++ bin/windows/deploy.bat
@@ -12,6 +12,9 @@ git commit -m "Automated commit"
 git push
 git checkout gh-pages
 git merge origin
+cp ../greatwalks-android/bin/greatwalks-android.apk .
+git add -A
+git commit -m "Automated commit"
 git push origin gh-pages
 git checkout master
 cd ../greatwalks-android
