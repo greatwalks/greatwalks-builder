743c88e4d16036a473f9519d9b5a702e87175556
Stuart McDonald <stuart@catalyst.net.nz>
2012-12-04 16:17:24 +1300

    Added js for other offers links

--- javascript/offers.js
+++ javascript/offers.js
@@ -2,9 +2,10 @@
     "use strict";
     var make_blank = function() {
         if ( navigator.userAgent.match(/iphone|ipad|ipod/i) ) {
-            $('ul.banners li a').attr('target', "_blank");
+            $('ul.banners li a, div.offers li a').attr('target', "_blank");
         }
     }
 
     window.pageload(make_blank, '/offers');
+    window.pageload(make_blank, '/walk-');
 }(jQuery));
