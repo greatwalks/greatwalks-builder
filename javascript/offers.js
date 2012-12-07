64a412e971e58c4b0c8696e599f636d1e6bdfa2b
Matthew Holloway <matth@catalyst.net.nz>
2012-12-07 14:45:35 +1300

    Automated commit

--- javascript/offers.js
+++ javascript/offers.js
@@ -4,7 +4,7 @@
         if ( navigator.userAgent.match(/iphone|ipad|ipod/i) ) {
             $('ul.banners li a, div.offers li a').attr('target', "_blank");
         }
-    }
+    };
 
     window.pageload(make_blank, '/offers');
     window.pageload(make_blank, '/walk-');
