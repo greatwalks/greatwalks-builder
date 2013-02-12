c43396680c83eca874d21a26ba9e6f2048bdcef5
Matthew Holloway <matth@catalyst.net.nz>
2013-02-13 10:59:09 +1300

    Automated commit

--- bin/linux/deploy.sh
+++ bin/linux/deploy.sh
@@ -10,21 +10,21 @@ git add -A
 git commit -m "Automated commit"
 git push
 cd ../greatwalks
-#git checkout master
-#git pull
-#git add -A
-#git commit -m "Automated commit"
-##git push
-#git checkout gh-pages
-#git merge origin
-#cp ../greatwalks-android/bin/greatwalks-android.apk .
-#git add -A
-#git commit -m "Automated commit"
-#git push origin gh-pages
-#git checkout master
-#cd ../greatwalks-android
-#git submodule foreach git pull origin master
-#git add -A
-#git commit -m "Automated commit"
-#git push
-#cd ../greatwalks-builder
+git checkout master
+git pull
+git add -A
+git commit -m "Automated commit"
+git push
+git checkout gh-pages
+git merge origin
+cp ../greatwalks-android/bin/greatwalks-android.apk .
+git add -A
+git commit -m "Automated commit"
+git push origin gh-pages
+git checkout master
+cd ../greatwalks-android
+git submodule foreach git pull origin master
+git add -A
+git commit -m "Automated commit"
+git push
+cd ../greatwalks-builder
